355c130122bdaf801197c6aede57acc4d1f997d9
Mateus Winter <mateus.winter@edu.itajai.sc.gov.br>
2021-06-25 18:01:09 -0300

    Day 5

---
+++ src/configs/discordAuth.ts
@@ -0,0 +1,15 @@
+const REDIRECT_URI = 'https%3A%2F%2Fauth.expo.io%2Fgameplay';
+const SCOPE = 'identify%20email%20connections%20guilds';
+const RESPONSE_TYPE = 'token';
+const CLIENT_ID = '858039495399964682';
+const CDN_IMAGE = 'https://cdn.discordapp.com';
+
+//https://discord.com/api/oauth2/authorize?client_id=858039495399964682&redirect_uri=https%3A%2F%2Fauth.expo.io%2Fgameplay&response_type=code&scope=identify%20email%20connections%20guilds
+
+export {
+  REDIRECT_URI,
+  SCOPE,
+  RESPONSE_TYPE,
+  CLIENT_ID,
+  CDN_IMAGE
+}
